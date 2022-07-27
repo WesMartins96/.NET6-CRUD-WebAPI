842b515c9efc94ee3885160bfc603614dc5b3f0f
Wesley Martins <wesmartins96@gmail.com>
2022-07-27 14:43:38 -0300

    atualizacao tabela usuario

--- Data/UsuarioContext.cs
+++ Data/UsuarioContext.cs
@@ -14,5 +14,19 @@ namespace Crud_.NET6_WebApi.Data
         }
 
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            var usuario = modelBuilder.Entity<Usuario>();
+
+            usuario.ToTable("tb_usuario");
+
+            usuario.HasKey(u => u.Id);
+
+            usuario.Property(u => u.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            usuario.Property(u => u.Nome).HasColumnName("nome").IsRequired();
+            usuario.Property(u => u.DataNascimento).HasColumnName("data_nascimento");
+        }
     }
 }
